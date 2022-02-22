88bd12424c4cb8a9bf1ec7b94cce702e1824c86f
Pollogamerz <lexius002@gmail.com>
2022-02-22 08:25:07 -0600

    UpdateChamgesRecuested

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,42 +2,48 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    Rigidbody2D _rb2D;
-    [SerializeField] private float _velocity = 10f;
-    [SerializeField] private float _JumpH = 10f;  
-    [SerializeField] Transform _feetPos;
-    [SerializeField] float _checkRadius;
-    [SerializeField] LayerMask _whatIsGrnd;
-    [SerializeField] private float _jumptime;
-    private bool _isGrounded=default;
-    private bool _isJumping;
-    private float _jumpCounter;
+    Rigidbody2D _rigidBody2D;
+    [SerializeField] private float _movementVelocity = 10f;
+    [SerializeField] private float _JumpHeight = 10f;  
+    [SerializeField] Transform _feetPos = default;
+    [SerializeField] float _checkRadius = default;
+    [SerializeField] LayerMask _groundLayer = default;
+    [SerializeField] private float _jumpTime = default;
+    private bool _isGrounded = default;
+    private bool _isJumping = default;
+    private float _jumpCounter = default;
     
 
     void Start()
     {
-        _rb2D=GetComponent<Rigidbody2D>();   
+        _rigidBody2D = GetComponent<Rigidbody2D>();   
     }
 
     void Update()
     {
         if (Input.GetKey(KeyCode.A))
-            gameObject.transform.Translate(-_velocity * Time.deltaTime, 0, 0);
-        if (Input.GetKey(KeyCode.D))
-            gameObject.transform.Translate(_velocity * Time.deltaTime, 0, 0);
+        {
+            gameObject.transform.Translate(-_movementVelocity * Time.deltaTime, 0, 0);
+        }
 
-        _isGrounded = Physics2D.OverlapCircle(_feetPos.position, _checkRadius, _whatIsGrnd);
+        if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.Translate(_movementVelocity * Time.deltaTime, 0, 0);
+
+        }
+        _isGrounded = Physics2D.OverlapCircle(_feetPos.position, _checkRadius, _groundLayer);
         if (_isGrounded == true && Input.GetKeyDown(KeyCode.Space))
         {
             _isJumping = true;
-            _jumpCounter = _jumptime;
-            _rb2D.velocity = Vector2.up * _JumpH;
+            _jumpCounter = _jumpTime;
+            _rigidBody2D.velocity = Vector2.up * _JumpHeight;
         }
+
         if (Input.GetKey(KeyCode.Space) && _isJumping == true)
         {
             if (_jumpCounter > 0)
             {
-                _rb2D.velocity = Vector2.up * _JumpH;
+                _rigidBody2D.velocity = Vector2.up * _JumpHeight;
                 _jumpCounter -= Time.deltaTime;
             }
             else 
@@ -46,9 +52,11 @@ public class Movement : MonoBehaviour
             }
             
         }
+
         if (Input.GetKeyUp(KeyCode.Space))
         {
             _isJumping = false;
         }
     }
 }
+
