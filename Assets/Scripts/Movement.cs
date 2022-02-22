bb619240cfc1a930b26e8a457aefe045adbe2f4a
Pollogamerz <lexius002@gmail.com>
2022-02-22 07:58:50 -0600

    ImprovementInJump

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,9 +2,17 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    private float _velocity = 10f;
-    [SerializeField] private float _jumpH = 4f;
     Rigidbody2D _rb2D;
+    [SerializeField] private float _velocity = 10f;
+    [SerializeField] private float _JumpH = 10f;  
+    [SerializeField] Transform _feetPos;
+    [SerializeField] float _checkRadius;
+    [SerializeField] LayerMask _whatIsGrnd;
+    [SerializeField] private float _jumptime;
+    private bool _isGrounded=default;
+    private bool _isJumping;
+    private float _jumpCounter;
+    
 
     void Start()
     {
@@ -17,8 +25,30 @@ public class Movement : MonoBehaviour
             gameObject.transform.Translate(-_velocity * Time.deltaTime, 0, 0);
         if (Input.GetKey(KeyCode.D))
             gameObject.transform.Translate(_velocity * Time.deltaTime, 0, 0);
-        if (Input.GetKeyDown(KeyCode.Space))
-           _rb2D.AddForce(new Vector2(0, _jumpH), ForceMode2D.Impulse);
 
+        _isGrounded = Physics2D.OverlapCircle(_feetPos.position, _checkRadius, _whatIsGrnd);
+        if (_isGrounded == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            _isJumping = true;
+            _jumpCounter = _jumptime;
+            _rb2D.velocity = Vector2.up * _JumpH;
+        }
+        if (Input.GetKey(KeyCode.Space) && _isJumping == true)
+        {
+            if (_jumpCounter > 0)
+            {
+                _rb2D.velocity = Vector2.up * _JumpH;
+                _jumpCounter -= Time.deltaTime;
+            }
+            else 
+            {
+                _isJumping = false;
+            }
+            
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            _isJumping = false;
+        }
     }
 }
