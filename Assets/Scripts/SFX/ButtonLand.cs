f0d74d8c37de1b3117126e37fbcfaf3da31b296a
Lufrauwu <lexius002@gmail.com>
2022-06-09 22:07:55 -0500

    MainMenuMusic

---
+++ Assets/Scripts/SFX/ButtonLand.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonLand : MonoBehaviour
+{
+    public AudioSource buttonland;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ButtonLandingSound()
+    {
+        buttonland.Play();
+    }
+}
